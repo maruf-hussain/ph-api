315eb299e7488c8a3690fb63301f9f595da20611
maruf-hussain <engmarufhussain@gmail.com>
2025-08-21 14:44:17 +0600

    dljf

--- video.js
+++ video.js
@@ -29,4 +29,35 @@ const displayCategories = async (data) => {
   })
 }
 
+
+// ........Load videos...............................
+const loadVideos = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    const data = await res.json()
+    displayVideos(data.videos)
+}
+
+// ............display videos......................
+
+const displayVideos = (data) => {
+   const videoContainer = document.getElementById('videoContainer')
+    videoContainer.innerHTML = '' // Clear previous content
+    data.forEach( video => {
+        
+        const videoCard = document.createElement('div')
+        videoCard.classList.add( 'bg-blue-100','mt-6', 'rounded-lg', 'shadow-lg', 'p-4', 'flex', 'flex-col', 'items-center')
+        videoCard.innerHTML = `
+                <img src="${video.thumbnail}" alt="">
+                <h2 class="font-bold text-2xl py-2 text-red-900">${video.title}</h2>
+                <p>${video.description}</p>
+               
+                 
+        `
+        videoContainer.appendChild(videoCard)
+      
+       
+    })
+}
+
 loadCategories()
+loadVideos()
